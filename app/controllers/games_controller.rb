afb006682e5eeb935ca1bd3388df9f90ee01bc14
Anais Augu <augu.anais@gmail.com>
2022-02-23 00:41:50 +0100

    validations

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,18 @@
 class GamesController < ApplicationController
   def new
+    @categories = []
+    @console_collection = ["Atari", "SuperNintendo", "DreamCast", "Sega", "GameBoy", "NintendoDS", "Switch", "PC", "XBox", "Xbox360", "PSVita", "PSOne", "PS2", "PS3", "PS4", "PS5"]
     @game = Game.new
   end
 
-  # def create
+  def create
+    @game = Game.new(game_params)
+    @game.user = current_user
 
-  # end
+    if @game.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
 end
