bd911356c0c95c526326cfd15a27b20011f51778
Anais Augu <augu.anais@gmail.com>
2022-03-01 20:46:02 +0100

    new booking view ok

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,25 @@
 class BookingsController < ApplicationController
   def new
+    @game = Game.find(params[:game_id])
     @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @game = Game.find(params[:game_id])
+    @booking.user = current_user
+    @booking.game = @game
+
+    if @booking.save
+      redirect_to dashboards_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
