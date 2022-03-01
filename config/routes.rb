dbf0e4e6033ef776baa289dffe3a21d29c8870c2
Anais Augu <augu.anais@gmail.com>
2022-03-01 20:01:52 +0100

    routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "pages#home"
-  resources :games, only: [:index, :show, :new, :create]
+  resources :games, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:new, :create]
+  end
 end
