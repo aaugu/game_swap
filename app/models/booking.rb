3f8335d8ace63d7a2d15d925a37099e887ef1a96
Anais Augu <augu.anais@gmail.com>
2022-03-01 19:59:12 +0100

    validations on model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,21 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :game
+
+  validates :start_date, :end_date, presence: true
+  validate :booking_dates_valid?
+
+  private
+
+  def booking_dates_valid?
+    return if end_date.blank? || start_date.blank?
+
+    if start_date < Date.today
+      errors.add(:start_date, "Start date must be after today")
+    elsif end_date < Date.today
+      errors.add(:end_date, "End date must be after today")
+    elsif end_date < start_date
+      errors.add(:end_date, "Must be after the start date")
+    end
+  end
 end
